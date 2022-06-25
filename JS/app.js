1c6086e41192820029619ea93f58f0c692744140
jhendricks200 <72288418+jhendricks200@users.noreply.github.com>
2022-06-24 21:39:31 -0400

    recent

--- JS/app.js
+++ JS/app.js
@@ -4,8 +4,7 @@ const alertBanner = document.getElementById("alert");
 // Create HTML for banner
 alertBanner.innerHTML = `
 <div class="alert-banner">
-<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
-to complete</p>
+<p><strong>Alert:</strong> You have unread messages</p>
 <p class="alert-banner-close">x</p>
 </div>
 `;
